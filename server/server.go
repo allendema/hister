0f39cb37568c4402bfd750025c55d89d3155e767
Adam Tauber <asciimoo@gmail.com>
2026-02-13 14:33:49 +0100

    [fix] do not process documents if there is a matching skip rule

--- server/server.go
+++ server/server.go
@@ -357,12 +357,12 @@ func serveAdd(c *webContext) {
 		d.Title = f.Get("title")
 		d.Text = f.Get("text")
 	}
-	if err := d.Process(); err != nil {
-		log.Error().Err(err).Str("URL", d.URL).Msg("failed to process document")
-		serve500(c)
-		return
-	}
 	if !c.Config.Rules.IsSkip(d.URL) && !strings.HasPrefix(d.URL, c.Config.BaseURL("/")) {
+		if err := d.Process(); err != nil {
+			log.Error().Err(err).Str("URL", d.URL).Msg("failed to process document")
+			serve500(c)
+			return
+		}
 		err := indexer.Add(d)
 		if err != nil {
 			log.Error().Err(err).Str("URL", d.URL).Msg("failed to create index")
